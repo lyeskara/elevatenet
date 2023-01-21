5d86bbc56e17e0b3fad0bd9523eae22b9c69b961
Lyes Kara <lyeskara1106@gmail.com>
2023-01-21 14:33:48 -0500

    creation of registration function

--- frontend/src/components/Navbar/JoinNow.js
+++ frontend/src/components/Navbar/JoinNow.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React , {useState}from 'react'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import { auth } from '../../firebase.js'
+
+
 
 function JoinNow() {
+  const [email, setEmail] = new useState("")
+  const [password, setPassword] = new useState("")
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+ 
   return (
-    <div>
-      <h1>rtht</h1>
-    </div>
+    <form >
+    <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    </label>
+    <br />
+    <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    </label>
+    <br />
+        <button  onClick={register}>Register</button>
+</form>
   )
 }
 
