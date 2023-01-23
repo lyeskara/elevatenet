5e1fcbb64e40a0736ec8e1ce236b0be0754f0001
Lyes Kara <lyeskara1106@gmail.com>
2023-01-23 10:10:47 -0500

    added a wrapper around the main routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import './styles/App.css';
 import {Route , Routes} from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar';
-import SignIn from './components/Navbar/SignIn';
-import JoinNow from './components/Navbar/JoinNow';
-import Linkedin from './components/Navbar/Linkedin';
+import Navbar from './components/Navbar';
+import SignIn from './components/authentication/SignIn';
+import JoinNow from './components/authentication/JoinNow';
+import Linkedin from './components/Linkedin';
+import ProfileForm from './components/ProfileInfo/ProfileForm';
+import Protection from './context/Protection'
+import { UserAuthContextProvider } from './context/UserAuthContext';
+
+
 function App() {
   return (
-    <>
+    <UserAuthContextProvider>
     <Navbar/>
     <Routes>
+    <Route path="/ProfileForm" element={<Protection>{<ProfileForm/>}</Protection>}></Route>
     <Route path="/" element={<Linkedin />} />
     <Route path="/SignIn" element={<SignIn/>} />
     <Route path="/JoinNow" element={<JoinNow/>} />
     </Routes> 
-    </> 
+    </UserAuthContextProvider>
     );
 }
 
