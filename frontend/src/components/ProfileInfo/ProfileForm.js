9a41224ff3e590eca3b280557f6b6d263305a447
Lyes Kara <lyeskara1106@gmail.com>
2023-01-23 10:11:18 -0500

    updates

---
+++ frontend/src/components/ProfileInfo/ProfileForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfileForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfileForm
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -19,3 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
