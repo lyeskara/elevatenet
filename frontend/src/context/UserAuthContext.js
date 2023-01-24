d89510ce657fbda66f2d87d40c2141ecfb4fd4ba
Lyes Kara <lyeskara1106@gmail.com>
2023-01-24 12:06:50 -0500

    updates that make user stored in local storage

--- frontend/src/context/UserAuthContext.js
+++ frontend/src/context/UserAuthContext.js
@@ -1,31 +1,44 @@
-import { createContext, useContext, useEffect,useState } from 'react';
+import { createContext, useContext } from 'react';
 import { createUserWithEmailAndPassword,
           signInWithEmailAndPassword,
-          signOut,
-          onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../firebase';
+          setPersistence,  signOut,
 
+       } from 'firebase/auth';
+import { auth } from '../firebase';
+import { browserSessionPersistence } from 'firebase/auth';
 
 const UserAuthContext = createContext();
   
 export function UserAuthContextProvider({children}){
-   const [user,setUser] = useState("");
    function Registration(email,password){
     return createUserWithEmailAndPassword(auth,email,password);
    }
-   function Login(email,password){
-    return signInWithEmailAndPassword(auth,email,password);
-   }
-   useEffect(()=>{
-      const unsubscribe = onAuthStateChanged(auth, (current_user)=>{
-                 setUser(current_user);
-       })
-       return ()=>{
-        unsubscribe();
-       }
-   },[])
 
-   const value = {user,Login,Registration}
+
+   function Login(email,password){
+      setPersistence(auth, browserSessionPersistence)
+      .then(() => {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        return signInWithEmailAndPassword(auth, email, password);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+      console.log(error.message);
+      });   
+   }
+   function logOut() {
+      return signOut(auth);
+    }   
+
+
+
+
+
+   const value = {Login,Registration,logOut}
    return<UserAuthContext.Provider value={value}> {children}</UserAuthContext.Provider>
 }
 
