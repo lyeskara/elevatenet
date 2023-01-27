367af2487c43cca14c0d33307cdad1f12d70a650
Lyes Kara <lyeskara1106@gmail.com>
2023-01-26 21:43:27 -0500

    updates to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react'
-import {Link,Outlet} from 'react-router-dom'
-
+import {Link,Outlet,useNavigate} from 'react-router-dom'
+import '../styles/nav.css'
+import { useUserAuth } from '../context/UserAuthContext'
+import { auth } from '../firebase'
 function Navbar() {
+  const userr = auth.currentUser;
+  const {logOut} = useUserAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <>
     <nav>
@@ -9,17 +22,31 @@ function Navbar() {
         <li>
           <Link to="/">Linkedin</Link>
         </li>
-        <li>
-          <Link to="/SignIn">Sign In</Link>
-        </li>
+      {(userr) &&  <> 
+      <li>
+      <Link to="/Profile">Profile</Link>
+      <button onClick={handleLogout} className="list-item">logout</button>
+
+    </li> 
+    </>
+}
+    {!userr && <>
+     <li>
+        <Link to="/SignIn">Sign In</Link>
+     </li>
         <li>
           <Link to="/JoinNow">Join Now</Link>
         </li>
+     </>
+    }
+
+      
     </ul>
     </nav>
           <Outlet />
           </>
   )
+
 }
 
 export default Navbar
