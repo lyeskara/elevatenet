bac5a1267e1d7539304104205c413cb08e64ab6a
Lyes Kara <lyeskara1106@gmail.com>
2023-01-26 21:48:26 -0500

    updates to the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import SignIn from './components/authentication/SignIn';
 import JoinNow from './components/authentication/JoinNow';
 import Linkedin from './components/Linkedin';
 import ProfileForm from './components/ProfileInfo/ProfileForm';
+import Profile from './components/ProfileInfo/Profile';
 import Protection from './context/Protection'
 import { UserAuthContextProvider } from './context/UserAuthContext';
 
@@ -15,9 +16,11 @@ function App() {
     <Navbar/>
     <Routes>
     <Route path="/ProfileForm" element={<Protection>{<ProfileForm/>}</Protection>}></Route>
+    <Route path="/Profile" element={<Protection>{<Profile/>}</Protection>}></Route>
     <Route path="/" element={<Linkedin />} />
     <Route path="/SignIn" element={<SignIn/>} />
     <Route path="/JoinNow" element={<JoinNow/>} />
+
     </Routes> 
     </UserAuthContextProvider>
     );
