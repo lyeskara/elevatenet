723960dc13d44010ba83656a014805d288d2396f
julietrinh1 <67767875+julietrinh1@users.noreply.github.com>
2023-02-11 01:51:25 -0500

    Front page outline done
    This is to have something nice to look at

--- frontend/src/components/Linkedin.js
+++ frontend/src/components/Linkedin.js
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import gif from './../images/test.gif';
 
 function Linkedin() {
   return (
-    <div>
-            <h2 class="">Bring your career to new heights</h2>
-            <h1>The platform <br></br> Where interests <br></br> become friendships </h1>
+    <div class="container">
 
+            <p class="slogan">Bring your career to new <span class="green">heights</span> </p>
+            <p class ="frontCatchPhrase">The platform <br></br> Where <span class="green">interests</span> <br></br> become <span class="green">friendships </span></p>
+            <p class ="catch">Whatever your interest, from hiking and reading to networking<br></br> and skill sharing, there are thousands of people who share it on<br></br> Meetup. Events are happening every day—log in to join the fun.</p>
+            <img class="gif" src={gif} alt='gif'></img>
     </div>
   )
 }
