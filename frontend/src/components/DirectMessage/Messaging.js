847a0b81e234206baedac468eb8c83132b423d19
Zahin Khan <77297140+ZahinK7@users.noreply.github.com>
2023-02-14 01:05:34 -0500

    Displaying messages to website

--- frontend/src/components/DirectMessage/Messaging.js
+++ frontend/src/components/DirectMessage/Messaging.js
@@ -1,47 +1,50 @@
+import React, { useState, useRef } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
-import React, { useRef  } from 'react';
 
 const Message = () => {
-
-//For sending messages
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const messageRef = useRef();
-  const ref = collection(db, "messages");
+  const ref = collection(db, 'messages');
 
-//For retreving messages
-  
-  //On send, the msg is sent to firebase
   const handleSubmit = (e) => {
-    //to not reload when send is clicked
     e.preventDefault();
-
-    let data = {
-        message:messageRef.current.value,
+    const data = {
+      message: messageRef.current.value,
     };
-    try{
-      addDoc(ref,data)
-    }catch(e){
+    try {
+      addDoc(ref, data);
+      setMessages([...messages, messageRef.current.value]); // Add the message to the messages array
+      setMessage(''); // Clear the message input after sending the message
+    } catch (e) {
       console.log(e);
     }
+  };
 
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
   };
 
   return (
     <div>
       <h2>Messages</h2>
-      {/* <ul>
-        {message.map((m, index) => (
-          <li key={index}>{m}</li>
-        ))}
-      </ul> */}
+
       <form onSubmit={handleSubmit}>
         <label>Write a message...</label>
-        <input type="text" 
-       
-        ref={messageRef}
-         />
+        <input
+          type="text"
+          value={message}
+          onChange={handleMessageChange}
+          ref={messageRef}
+        />
         <button type="submit">Send</button>
       </form>
+
+      {/* Display the messages */}
+      {messages.map((msg, index) => (
+        <p key={index}>{msg}</p>
+      ))}
     </div>
   );
 };
