2245126c1a65d7940c972ad384b5428319db5ffe
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2023-02-14 00:50:44 -0500

    maping messages in comment

--- frontend/src/components/DirectMessage/Messaging.js
+++ frontend/src/components/DirectMessage/Messaging.js
@@ -1,10 +1,14 @@
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
-import React, { useRef } from 'react';
+import React, { useRef , useState } from 'react';
 
 const Message = () => {
+
+//For sending messages
   const messageRef = useRef();
   const ref = collection(db, "messages");
+
+//For retreving messages
   
   //On send, the msg is sent to firebase
   const handleSubmit = (e) => {
@@ -25,10 +29,15 @@ const Message = () => {
   return (
     <div>
       <h2>Messages</h2>
-   
+      {/* <ul>
+        {message.map((m, index) => (
+          <li key={index}>{m}</li>
+        ))}
+      </ul> */}
       <form onSubmit={handleSubmit}>
         <label>Write a message...</label>
         <input type="text" 
+       
         ref={messageRef}
          />
         <button type="submit">Send</button>
