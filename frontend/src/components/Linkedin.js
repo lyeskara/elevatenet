ffd4930de5eaebbf7720514562964b9f135345fb
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2023-02-14 00:54:59 -0500

    Change class to className and it works

--- frontend/src/components/Linkedin.js
+++ frontend/src/components/Linkedin.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Linkedin() {
   return (
     <div>
-            <h2 class="">Bring your career to new heights</h2>
+            <h2 className="">Bring your career to new heights</h2>
             {/* <h2 className="">Bring your career to new heights</h2> */}
             <h1>The platform <br></br> Where interests <br></br> become friendships </h1>
 
