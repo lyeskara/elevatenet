bd09b3f17155c0fcbc912bc69bded737053138ab
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2023-02-13 23:35:23 -0500

    updated working code with firebase in messaging.js

--- frontend/src/components/DirectMessage/Messaging.js
+++ frontend/src/components/DirectMessage/Messaging.js
@@ -1,62 +1,38 @@
-//Uncomment code to use these imports
-
-// import { collection, doc, onSnapshot } from 'firebase/firestore';
-// import { auth, db } from '../../firebase';
-//import React, { useState, useEffect } from 'react';
-import React, { useState} from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+import React, { useRef } from 'react';
 
 const Message = () => {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-
-  //Uncomment code to use Firebase function (must uncomment imports first) like onSnapshot
-
-  // useEffect(() => {
-  //   const messagesRef = collection(db, "messages");
-  //   const userDocRef = doc(messagesRef, auth.currentUser.uid);
-
-  //   // Listen for updates to the user's messages
-  //   const unsubscribe = onSnapshot(userDocRef, (doc) => {
-  //     if (doc.exists()) {
-  //       setMessages(doc.data().messages);
-  //     }
-  //   });
-
-  //   // Unsubscribe from real-time updates when the component unmounts
-  //   return unsubscribe;
-  // }, []);
-
+  const messageRef = useRef();
+  const ref = collection(db, "messages");
+  
+  //On send, the msg is sent to firebase
   const handleSubmit = (e) => {
+    //to not reload when send is clicked
     e.preventDefault();
-  
-    //Uncomment code to use Firebase db (must uncomment import first)
 
-    // const messagesRef = collection(db, "messages");
-    // const userDocRef = doc(messagesRef, auth.currentUser.uid);
+    let data = {
+        message:messageRef.current.value,
+    };
+    try{
+      addDoc(ref,data)
+    }catch(e){
+      console.log(e);
+    }
 
-    // // Write the message to the Firestore database
-    // userDocRef.set({
-    //   messages: [...messages, message]
-    // });
-  
-    setMessages([...messages, message]);
-    setMessage('');
   };
 
   return (
     <div>
       <h2>Messages</h2>
-      <ul>
-        {messages.map((m, index) => (
-          <li key={index}>{m}</li>
-        ))}
-      </ul>
+   
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
+        <label>Write a message...</label>
+        <input type="text" 
+        //value={message} 
+       // onChange={(e) => setMessage(e.target.value)} 
+        ref={messageRef}
+         />
         <button type="submit">Send</button>
       </form>
     </div>
