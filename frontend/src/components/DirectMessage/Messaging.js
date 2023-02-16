5d02a24668f9876407f999b4d29f370f4af7e8f6
Zahin Khan <77297140+ZahinK7@users.noreply.github.com>
2023-02-16 15:37:58 -0500

    Displays older messages sent to Firebase DB in chronological order

--- frontend/src/components/DirectMessage/Messaging.js
+++ frontend/src/components/DirectMessage/Messaging.js
@@ -1,12 +1,25 @@
-import React, { useState, useRef } from 'react';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import React, { useState, useRef, useEffect } from 'react';
+import { collection, addDoc, onSnapshot, serverTimestamp, query, orderBy } from 'firebase/firestore';
 import { db } from '../../firebase';
 
 const Message = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const messageRef = useRef();
-  const ref = collection(db, 'messages');
+  const messagesRef = collection(db, 'messages');
+
+  // Fetch old messages from Firestore and sort them by date when the component mounts
+  useEffect(() => {
+    const q = query(messagesRef, orderBy('createdAt'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const messagesArray = [];
+      querySnapshot.forEach((doc) => {
+        messagesArray.push(doc.data());
+      });
+      setMessages(messagesArray);
+    });
+    return unsubscribe;
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,9 +28,9 @@ const Message = () => {
       createdAt: serverTimestamp()
     };
     try {
-      addDoc(ref, data);
-      setMessages([...messages, messageRef.current.value]); // Add the message to the messages array
-      setMessage(''); // Clear the message input after sending the message
+      addDoc(messagesRef, data);
+      setMessages([...messages, data]);
+      setMessage('');
     } catch (e) {
       console.log(e);
     }
@@ -42,9 +55,8 @@ const Message = () => {
         <button type="submit">Send</button>
       </form>
 
-      {/* Display the messages */}
       {messages.map((msg, index) => (
-        <p key={index}>{msg}</p>
+        <p key={index}>{msg.text}</p>
       ))}
     </div>
   );
