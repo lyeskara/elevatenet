fe2a5bcdc9dca3f8f07c281fbe21b9769d0153c1
LeonZhang7 <zhang.leon07@gmail.com>
2023-02-18 02:48:49 -0500

    update on seeing the postings

--- frontend/src/components/Jobs/JobPostings.js
+++ frontend/src/components/Jobs/JobPostings.js
@@ -10,42 +10,85 @@ import Button from 'react-bootstrap/Button';
 
 
 function JobPostings() {
-	
-	const [user, setUser] = useState({});
 	const handleClick = () => {
 		window.location.href = '/CreateNewPosting';
 	  };
 	const handleClickJobPostings = () => {
 		window.location.href = '/JobPostings';
 	  };
+//=================================================================================================================
+	const [user, setUser] = useState({});
 
+	useEffect(() => {
+		async function getData() {
+			await getDoc(
+				doc(collection(db, "posting"))
+			)
+				.then((doc) => {
+					console.log("doc");
+					if (doc.exists) {
+						console.log(doc.data + " ");
+						// setUser({ ...doc.data(), id: doc.id });
+					} else {
+						console.log("nikmok");
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+		console.log(getData());
 
-	
+		return () => {
+			getData();
+		};
+	}, []);	  
+//=================================================================================================================
+	// const docRef = doc(db, "posting");
+	// //const docSnap = await getDoc(docRef);
 
-	const Fetchdata = ()=>{
-        db.collection("posting").get().then((querySnapshot) => {
+	// async function fetchDoc() {
+	// 	const docRef = doc(db, "my-collection", "my-doc");
+	// 	const docSnap = await getDoc(docRef);
+	// 	console.log(docSnap.data());
+	//   }
+	  
+	// //   fetchDoc();
+
+	// if (fetchDoc.exists()) {
+	// console.log("Document data:", fetchDoc.data());
+	// } else {
+	// // doc.data() will be undefined in this case
+	// console.log("No such document!");
+	// } 
+//=================================================================================================================
+	// const [books, setBooks] = useState([]);
+	// const fetchBooks = async () => {
+	// 	const req = await getDoc("posting").get();
+	// 	console.log(req)
+	// }
+
+	// useEffect(() =>{
+	// 	fetchBooks();
+	// }, [])
+//=================================================================================================================
+	// const [info , setInfo] = useState([]);
+	// window.addEventListener('load',() => {
+	// 	Fetchdata();
+	// });
+
+	// const Fetchdata = ()=>{
+    //     db.collection("posting").get().then((querySnapshot) => {
             
-            // Loop through the data and store
-            // it in array to display
-            querySnapshot.forEach(element => {
-                var data = element.data();
-                setInfo(arr => [...arr , data]);
+    //         // Loop through the data and store
+    //         // it in array to display
+    //         querySnapshot.forEach(element => {
+    //             var data = element.data();
+    //             setInfo(arr => [...arr , data]);
                  
-            });
-        })
-    }
-
-	const [info , setInfo] = useState([]);
-	window.addEventListener('load',() => {
-		Fetchdata();
-	});
-
-	// useEffect(() => {
-	// 	db.collection('posting').get().then((querySnapshot) => {
-	// 	  const data = querySnapshot.docs.map((doc) => doc.data());
-	// 	  setPostings(data);
-	// 	});
-	//   }, []);
+    //         });
+    //     })
+    // }
 //=================================================================================================================
 	return (
 		<Container className="container">
@@ -82,24 +125,12 @@ function JobPostings() {
     					</div> */}
 						{/* ================================ */}
 						{/* <div>
-							{postings.map((posting) => (
-								<div key={posting.job_title}>
-									<div className="posting">
-										<h3>{posting.job_title}</h3>
-										<p>Company: {posting.company}</p>
-										<p>Deadline: {posting.deadline}</p>
-										<p>Description: {posting.description}</p>
-									</div>
-								</div>
-							))}
-						</div> */}
-						<div>
 							<center>
 							<h2>Job Posting</h2>
 							</center>
 						
 						{
-							info.map((data) => (
+							user.map((data) => (
 							<Frame 
 								job_title={data.job_title}
 								company={data.company}
@@ -108,7 +139,7 @@ function JobPostings() {
 							))
 						}
 						</div>
-
+						<p>{user.job_title}</p> */}
 
 					</Card>
 				</Col>
