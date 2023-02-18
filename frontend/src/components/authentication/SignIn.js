26d54f9435513f031bf607084d31cb4ae0921f5f
julietrinh1 <67767875+julietrinh1@users.noreply.github.com>
2023-02-17 20:44:55 -0500

    Completed styling for sign in and sign up page

--- frontend/src/components/authentication/SignIn.js
+++ frontend/src/components/authentication/SignIn.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { useUserAuth } from "../../context/UserAuthContext.js";
 import { useNavigate } from "react-router-dom";
-import {} from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
+import "../../styles/JoinPages.css";
+import { GoogleAuthProvider } from "firebase/auth";
+import { signInWithGoogle } from "../../firebase.js";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { Login } = useUserAuth();
   const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,36 +26,82 @@ const SignIn = () => {
 
   return (
     <>
-    <div>
-      <p className="slogan">Bring your career to new heights</p>
-    </div>
-    
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <button type="submit"> Agree & Join</button>
-    </form>
-    </div>
+      <Container fluid>
+        <div>
+          <p className="text-center slogan pt-4 pb-3">
+            Bring your career to new heights
+          </p>
+        </div>
+
+        <div className="text-center containerForm">
+          <Form onSubmit={handleSubmit}>
+            <Form.Text className="sign center">Sign In</Form.Text>
+            <div>
+              <input
+                className="input_radio mt-4"
+                type="radio"
+                id="recruiter"
+                name="age"
+                value="recruiter"
+              ></input>
+              <label for="recruiter" className="label_radio">I'm a Recruiter</label>
+              <input
+                className="input_radio"
+                type="radio"
+                id="seeker"
+                name="age"
+                value="seeker"
+              ></input>
+              <label for="seeker" className="label_radio">I'm a Job Seeker</label>
+            </div>
+            <Button
+              className="google_button sign_button mb-3 mt-4"
+            >
+              Continue with Google
+            </Button>
+            <p className="line">
+              <span className="span_line">OR</span>
+            </p>
+            <center>
+              <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
+                <Form.Control
+                  className="input_box"
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control
+                  className="input_box"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Form.Text className="text-muted">
+                  <a href="/" className="password">
+                    Forgot Password?
+                  </a>
+                </Form.Text>
+              </Form.Group>
+            </center>
+            <Button className="sign_button mb-3 mt-3" type="submit">
+              Sign In
+            </Button>
+            <br></br>
+            <Form.Text className="text-muted">
+              New to ElevateNet?{" "}
+              <a href="/JoinNow" className="join">
+                Create an account
+              </a>
+            </Form.Text>
+          </Form>
+        </div>
+      </Container>
     </>
-    
-    
   );
 };
 
