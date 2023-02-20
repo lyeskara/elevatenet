d3ddb3d847b78cf788ecdc6ca3c5d04a3e1df70d
Zahin Khan <77297140+ZahinK7@users.noreply.github.com>
2023-02-20 15:40:33 -0500

    Commented code explaining functions and certain parameters

--- frontend/src/components/DirectMessage/Messaging.js
+++ frontend/src/components/DirectMessage/Messaging.js
@@ -2,13 +2,21 @@ import React, { useState, useRef, useEffect } from 'react';
 import { collection, addDoc, onSnapshot, serverTimestamp, query, orderBy } from 'firebase/firestore';
 import { db } from '../../firebase';
 
+// useState is used to update the messages with the most recent one
+// message parameter is the message that has been recently inputed by the user
+// messages contains an array of messages that has been sent so far
+//useRef fetches inputed value
 const Message = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const messageRef = useRef();
   const messagesRef = collection(db, 'messages');
 
-  // Fetch old messages from Firestore and sort them by date when the component mounts
+// Fetch old messages from Firestore and sort them by date when the component mounts
+//useEffect function is used to get previous messages and sort them by date (newest to oldest) 
+//query function is used to retrieve data from Cloud Firestore
+//onSnapshot is a function that listens for the latest data in real-time, which in this case updates the array of messages
+//const unsubscribe prevents memory leaks of data when Firebase is not used
   useEffect(() => {
     const q = query(messagesRef, orderBy('createdAt'));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -20,6 +28,8 @@ const Message = () => {
     });
     return unsubscribe;
   }, []);
+
+// handleSubmit function creates a message object using the messageRef and serverTimestamp and send it to Firebase using the addDoc function
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -40,6 +50,7 @@ const Message = () => {
     setMessage(e.target.value);
   };
 
+  //map iterate through the array
   return (
     <div>
       <h2>Messages</h2>
