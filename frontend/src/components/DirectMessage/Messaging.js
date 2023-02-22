a136b9d5f52af343dc54b5c5883e6fa3ff5bf2aa
sharjanan <sharjanan55@hotmail.com>
2023-02-22 13:10:07 -0500

    messaging with 3 different users

--- frontend/src/components/DirectMessage/Messaging.js
+++ frontend/src/components/DirectMessage/Messaging.js
@@ -1,16 +1,21 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { collection, addDoc, onSnapshot, serverTimestamp, query, orderBy } from 'firebase/firestore';
+import { collection, addDoc, onSnapshot, serverTimestamp, query, orderBy, where } from 'firebase/firestore';
+import { auth } from "../../firebase";
 import { db } from '../../firebase';
+
 
 // useState is used to update the messages with the most recent one
 // message parameter is the message that has been recently inputed by the user
 // messages contains an array of messages that has been sent so far
 //useRef fetches inputed value
-const Message = () => {
+const Message = () => 
+{
+  const currentUser = auth.currentUser;
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const messageRef = useRef();
   const messagesRef = collection(db, 'messages');
+  const [recipientId, setRecipientId] = useState(null);
 
 // Fetch old messages from Firestore and sort them by date when the component mounts
 //useEffect function is used to get previous messages and sort them by date (newest to oldest) 
@@ -18,7 +23,18 @@ const Message = () => {
 //onSnapshot is a function that listens for the latest data in real-time, which in this case updates the array of messages
 //const unsubscribe prevents memory leaks of data when Firebase is not used
   useEffect(() => {
-    const q = query(messagesRef, orderBy('createdAt'));
+    const senderId = currentUser.uid;
+    console.log('Recipient ID:', recipientId);
+    if( recipientId !== null){
+    const q = query(messagesRef,
+       where ('sender', '==', senderId), 
+       where ('recipient', '==', recipientId), 
+       orderBy('createdAt'));
+    console.log(' query:', q);
+    console.log(' senderid:', senderId);
+    console.log(' recipientId:', recipientId);
+
+
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const messagesArray = [];
       querySnapshot.forEach((doc) => {
@@ -27,7 +43,7 @@ const Message = () => {
       setMessages(messagesArray);
     });
     return unsubscribe;
-  }, []);
+  }}, [recipientId]);
 
 // handleSubmit function creates a message object using the messageRef and serverTimestamp and send it to Firebase using the addDoc function
 
@@ -35,7 +51,10 @@ const Message = () => {
     e.preventDefault();
     const data = {
       text: messageRef.current.value,
-      createdAt: serverTimestamp()
+      createdAt: serverTimestamp(),
+      sender:currentUser.uid,
+      recipient:"WpubLzGEbHbgXhx4Bj1Bdh6mklg1"//leon
+      
     };
     try {
       addDoc(messagesRef, data);
@@ -48,20 +67,38 @@ const Message = () => {
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
+  };//map iterate through the array
+
+  const handlePersonClick = (e) => {
+    e.preventDefault();
+    const recipientId = e.target.getAttribute('data-recipient-id');
+    setRecipientId(recipientId);
   };
-
-  //map iterate through the array
+  
   return (
+    
     <div>
-      <h2>Messages</h2>
 
+      <h2>Chat List</h2>
+      <div className="container-left">
+      <div className="list">
+        <ul>
+          <li>Person 1:<a href="" onClick={handlePersonClick} data-recipient-id="WpubLzGEbHbgXhx4Bj1Bdh6mklg1"> WpubLzGEbHbgXhx4Bj1Bdh6mklg1</a></li>{/*leon*/}
+          <li>Person 2:<a href="" onClick={handlePersonClick} data-recipient-id="v7nXsR4QSiUi5fLgapTsXtmJOF62">v7nXsR4QSiUi5fLgapTsXtmJOF62</a></li>{/*fatema*/}
+          <li>Person 3:<a href="" onClick={handlePersonClick} data-recipient-id="5XAWmTROsXdhyqsqClXpfnXPIXm1">5XAWmTROsXdhyqsqClXpfnXPIXm1</a></li>{/*alan*/}
+        </ul>
+      </div>
+    </div>
+
+      <h2>Messages</h2>
+      <div className="container-left">
       <form onSubmit={handleSubmit}>
-        <label>Write a message...</label>
         <input
           type="text"
           value={message}
           onChange={handleMessageChange}
           ref={messageRef}
+          placeholder="Write a message..."
         />
         <button type="submit">Send</button>
       </form>
@@ -69,6 +106,7 @@ const Message = () => {
       {messages.map((msg, index) => (
         <p key={index}>{msg.text}</p>
       ))}
+      </div>
     </div>
   );
 };
