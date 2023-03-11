eaac82fed71394ad9296a8b0df6cdacd0f3cad6d
LeonZhang7 <zhang.leon07@gmail.com>
2023-03-11 17:52:06 -0500

    Fixed Edit and Delete job posting not showing the postings afterwards
    - [done] Fixed Edit and Delete job posting not showing the postings afterwards
    - [todo] have to fix the same issue for "cancel" button in create job posting page

--- frontend/src/components/Jobs/JobPostings.js
+++ frontend/src/components/Jobs/JobPostings.js
@@ -38,6 +38,7 @@ function JobPostings() {
 	const handleDelete = async(id) =>{
 		try {
 			await deleteDoc(doc(db, "posting", id));
+			//window.location.reload(); // Reload the page after deleting the post
 		  } catch (error) {
 			console.error("Error deleting document: ", error);
 		  }
