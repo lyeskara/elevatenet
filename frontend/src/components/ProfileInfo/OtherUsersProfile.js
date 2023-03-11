3b6f8877771ab54c0013f7e68145126e847456ae
Lyes Kara <lyeskara1106@gmail.com>
2023-03-10 20:22:46 -0500

    fixed a bug in which caused the visited profile to not properly render the data from the firestore database

--- frontend/src/components/ProfileInfo/OtherUsersProfile.js
+++ frontend/src/components/ProfileInfo/OtherUsersProfile.js
@@ -15,142 +15,139 @@
  */
 
 
-import {React,useState,useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import {getDoc,query,where,getDocs, setDoc,collection,doc,updateDoc, documentId} from 'firebase/firestore';
+import { React, useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getDoc, query, where, getDocs, setDoc, collection, doc, updateDoc, documentId } from 'firebase/firestore';
 import { auth, db } from '../../firebase';
 
 function OtherUsersProfile() {
-  
-  const [follow,setfollow] = useState(false)
-  const {id} = useParams();
-  const [user, setUser] =useState({})
+
+  const [follow, setfollow] = useState(false)
+  const { id } = useParams();
+  const [user, setUser] = useState({})
   const currId = auth.currentUser.uid
   const followedId = id
 
-  const handlefollow = async ()=>{
+  const handlefollow = async () => {
 
-    const followRef = collection(db,'connection_requests');
-    const authdoc = doc(followRef,currId)
-    const array =[]
-    const addDoc = await getDocs((followRef)).then((word)=>{
-       word.docs.forEach((doc)=>{
+    const followRef = collection(db, 'connection_requests');
+    const authdoc = doc(followRef, currId)
+    const array = []
+    const addDoc = await getDocs((followRef)).then((word) => {
+      word.docs.forEach((doc) => {
         array.push(doc.id)
-       })
-       const condition = array.includes(authdoc.id)
-         if(!condition){
-          setDoc(doc(followRef, currId), {requests
-            :[followedId]});
-         }
-         else{
-         const getFollowers = getDoc(authdoc).then((document)=>{
+      })
+      const condition = array.includes(authdoc.id)
+      if (!condition) {
+        setDoc(doc(followRef, currId), {
+          requests
+            : [followedId]
+        });
+      }
+      else {
+        const getFollowers = getDoc(authdoc).then((document) => {
           const followedUsers = document.data().requests;
-          if(!followedUsers.includes(followedId)){
+          if (!followedUsers.includes(followedId)) {
             followedUsers.push(followedId)
-            return updateDoc(doc(followRef, currId), {...document.data(), requests : followedUsers})
-        }else{
-          console.log("already followed!")
-        }
-         })
-         }
-  }).catch((error)=>{
-        console.log(error);
+            return updateDoc(doc(followRef, currId), { ...document.data(), requests: followedUsers })
+          } else {
+            console.log("already followed!")
+          }
+        })
+      }
+    }).catch((error) => {
+      console.log(error);
     })
     setfollow(true);
-    }
-  
-  
-  const handleunfollow = async ()=>{
-    const followRef = collection(db,'follows')
-    const authdoc = await getDoc(doc(followRef,currId)).then((word)=>{
-      if(word.exists){
+  }
+
+
+  const handleunfollow = async () => {
+    const followRef = collection(db, 'follows')
+    const authdoc = await getDoc(doc(followRef, currId)).then((word) => {
+      if (word.exists) {
         const followedUsers = word.data().followd
         console.log(followedUsers)
-        if(followedUsers.includes(followedId)){
+        if (followedUsers.includes(followedId)) {
           const updatedFollowedUsers = followedUsers.filter(userId => userId !== followedId);
           console.log(updatedFollowedUsers)
-          return updateDoc(doc(followRef, currId), {...word.data(), followd: updatedFollowedUsers})
+          return updateDoc(doc(followRef, currId), { ...word.data(), followd: updatedFollowedUsers })
         }
       }
-    }).catch((error)=>{
-         console.log(error);
+    }).catch((error) => {
+      console.log(error);
     })
     setfollow(false);
   }
- 
-  useEffect(
-    ()=>{
-          async function getData(){
-             await getDoc(doc(collection(db, "users_information"),id))
-              .then((doc)=>{
-                   if(doc.exists){
-                      setUser({...doc.data(), id: doc.id });
-                   }else{
-                      console.log("error")
-                   }
-              }).catch((error) => {
-                  console.log(error);
-                });            
-             }
-             return ()=>{
-              getData(); 
-             }
-            }
-     ,  [id])
 
-  
-    
-return (
-  <>
-{currId !== id ? 
-  !follow ? (
+  useEffect(
+    () => {
+      getDoc(doc(collection(db, "users_information"), id)).then(
+        (doc) => {
+          if (doc.exists) {
+            setUser({ ...doc.data(), id: doc.id });
+          } else {
+            console.log("error")
+          }
+        }).catch((error) => {
+          console.log(error);
+        });
+    }
+    , [id])
+
+
+
+  return (
     <>
-      <button onClick={handlefollow}>Follow</button>
-      {informations(user)}
+      {currId !== id ?
+        !follow ? (
+          <>
+            <button onClick={handlefollow}>Follow</button>
+            {informations(user)}
+          </>
+        ) : (
+          <>
+            <button onClick={handleunfollow}>Unfollow</button>
+            {informations(user)}
+          </>
+        ) : (
+          informations(user)
+        )
+      }
+
     </>
-  ) : (
-    <>
-      <button onClick={handleunfollow}>Unfollow</button>
-      {informations(user)}
-    </>
-  ) : (
-    informations(user)
   )
 }
 
-    </>
-)
-}
-
-function informations(user){
+function informations(user) {
   return (
     <div className="user-profile">
-    <h1 className="name">{user.firstName} {user.lastName}</h1>
-    <p className="bio">{user.bio}</p>
-    <div className="education">
-      <h2>Education</h2>
-      <ul>
-        <li>{user.education}</li>
-      </ul>
-    </div>
-    <div className="work-experience">
-      <h2>Work Experience</h2>
-      <ul>
-        <li> {user.workExperience}</li>
-      </ul>
-    </div>
-    <div className="skills">
-      <h2>Skills</h2>
-      <ul>
-        <li>{user.skills}</li>
-      </ul>
-    </div>
-    <div className="languages">
-      <h2>Languages</h2>
-      <ul>
-        <li>{user.languages}</li>
-      </ul>
-    </div>
+      <h1 className="name">{user.firstName} {user.lastName}</h1>
+      <p className="bio">{user.bio}</p>
+      <div className="education">
+        <h2>Education</h2>
+        <ul>
+          <li>{user.education}</li>
+        </ul>
+      </div>
+      <div className="work-experience">
+        <h2>Work Experience</h2>
+        <ul>
+          <li> {user.workExperience}</li>
+        </ul>
+      </div>
+      <div className="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>{user.skills}</li>
+        </ul>
+      </div>
+      <div className="languages">
+        <h2>Languages</h2>
+        <ul>
+          <li>{user.languages}</li>
+        </ul>
+      </div>
     </div>
   )
 }
