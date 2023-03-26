5bc3d2060e75170f054501d450842492eb099cf1
LeonZhang7 <zhang.leon07@gmail.com>
2023-03-26 01:35:27 -0400

    Implementing placeholder content
    Implementing placeholder content in Advertisement page
    
    Co-Authored-By: fatema-a <78175202+fatema-a@users.noreply.github.com>

---
+++ frontend/src/components/Jobs/Advertisements.js
@@ -0,0 +1,19 @@
+// Importing necessary modules
+import React, { useEffect, useState } from "react";
+import { collection, query, where, getDoc, doc, onSnapshot, getDocs, deleteDoc, updateDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import Card from "react-bootstrap/Card";
+import "../../styles/JobPostings.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function Advertisements() {
+    return(
+        <h1>Advertisements</h1>
+    );
+    
+}
+export default Advertisements;
