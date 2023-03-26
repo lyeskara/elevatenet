fc521fcb96ef5d6d46b5527518c9652c6baa5760
julietrinh1 <67767875+julietrinh1@users.noreply.github.com>
2023-03-26 16:38:34 -0400

    Added a saved job page

--- frontend/src/components/Jobs/SavedJobs.js
+++ frontend/src/components/Jobs/SavedJobs.js
@@ -9,7 +9,7 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Button from 'react-bootstrap/Button';
 
-function JobPageForSeekers() {
+function SavedJobs() {
     const [postings, setPostings] = useState([]);
   
     useEffect(() => {
@@ -40,44 +40,30 @@ function JobPageForSeekers() {
 	};
     return (
       <>
-        <h1>Apply to Jobs</h1>
-        <Container>
+
+        <Container  className="container d-flex justify-content-center mx-auto">
         <Col xs={12} sm={8}  lg={4} style={{minWidth: "30%"}}>
 				{/* This card displays the job menu block with Job Postings and Advertisements */}
 				<Card className="jobs-menu">
 					<h2> Jobs </h2>
 					<hr></hr>
 					{/* When the user clicks the "Job Postings" text, it calls handleClickJobPostings */}
-					<h4 style={{ color: '#27746a' }}> Suggested Jobs </h4>
+					<h4 onClick={handleClickJobPostings}style={{ color: '#888888' }}> Suggested Jobs </h4>
 					{/* Advertisements */}
-					<h4 onClick={handleClickSavedJobs}  style={{ color: '#888888' }}> Saved Jobs </h4>
+					<h4 onClick={handleClickSavedJobs}  style={{ color: '#27746a' }}> Saved Jobs </h4>
 					<br></br>
 				</Card>
 			</Col>
+            <Col xs={12} sm={12}  lg={8} >
+				<h2 style={{ color: '#555555', marginTop: '32px' }}>Your Saved Jobs</h2>
+				
+			</Col>
           <Row>
-            {postings.map((posting) => (
-              <Col md={6} key={posting.id}>
-                <Card className="mb-3">
-                  <Card.Body>
-                    <Card.Title>{posting.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">
-                      {posting.company}
-                    </Card.Subtitle>
-                    <Card.Text>{posting.description}</Card.Text>
-                    <Card.Text>
-                        {posting.skills}
-                    </Card.Text>
-                    <Button variant="primary" style={{backgroundColor: "#27746A"}}>
-                        Apply Now
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
+            
           </Row>
         </Container>
       </>
     );
   }
   
-  export default JobPageForSeekers;
+  export default SavedJobs;
