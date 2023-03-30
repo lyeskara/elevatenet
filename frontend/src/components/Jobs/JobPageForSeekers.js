0ed2c31d1fbca553ca18903488a4ad08b109921e
LeonZhang7 <zhang.leon07@gmail.com>
2023-03-29 23:56:00 -0400

    Added carousel for advertisements
    Added carousel for 5 random advertisements showing at the end of the page

--- frontend/src/components/Jobs/JobPageForSeekers.js
+++ frontend/src/components/Jobs/JobPageForSeekers.js
@@ -9,6 +9,7 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
+import { Carousel } from 'react-bootstrap';
 
 function JobPageForSeekers() {
     const [postings, setPostings] = useState([]);
@@ -35,10 +36,36 @@ function JobPageForSeekers() {
   function handleRedirection(id){
     navigate(`/ApplyToJobs/${id}`)
   }
+
+    //having the job postings advertised, 5 random advertised job postings are picked to be displayed every load of page
+    const [postingsAD, setPostingsAD] = useState([]);
+  
+    useEffect(() => {
+      const postingsCollectionAD = collection(db, "posting");
+      const q = query(postingsCollectionAD, where("advertise", "in", [true, "on"]));
+  
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          docs.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        const shuffledDocs = docs.sort(() => 0.5 - Math.random());
+        const selectedDocs = shuffledDocs.slice(0, 5);
+        setPostingsAD(selectedDocs);
+      });
+  
+      return () => {
+        unsubscribe();
+      };
+    }, []);
     return (
       <>
         <h1>Apply to Jobs</h1>
         <Container>
+
           <Row>
             {postings.map((posting) => (
               <Col md={6} key={posting.id}>
@@ -60,6 +87,28 @@ function JobPageForSeekers() {
               </Col>
             ))}
           </Row>
+          <Row>
+            <h1>Sponsored</h1>
+            <Carousel>
+              {postingsAD.map((posting) => (
+                <Carousel.Item key={posting.id}>
+                  <Card className="mb-3">
+                    <Card.Body>
+                      <Card.Title>{posting.title}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        {posting.company}
+                      </Card.Subtitle>
+                      <Card.Text>{posting.description}</Card.Text>
+                      <Card.Text>{posting.skills}</Card.Text>
+                      <Button variant="primary" style={{backgroundColor: "#27746A"}}>
+                        Apply Now
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Row>          
         </Container>
       </>
     );
