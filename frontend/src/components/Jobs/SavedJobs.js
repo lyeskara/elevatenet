d8e4a951bfa79da573cb14491f5e28d0e0870dbc
julietrinh1 <67767875+julietrinh1@users.noreply.github.com>
2023-03-29 21:39:36 -0400

    display the saved posts

--- frontend/src/components/Jobs/SavedJobs.js
+++ frontend/src/components/Jobs/SavedJobs.js
@@ -1,69 +1,144 @@
+//This page is use to display the jobs that were saved in the JobPageForSeekers.js
+
 // Importing necessary modules
 import React, { useEffect, useState, useRef } from "react";
-import { collection, query, where, getDoc, doc, onSnapshot, getDocs, deleteDoc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDoc,
+  doc,
+  onSnapshot,
+  getDocs,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { auth, db } from "../../firebase";
 import Card from "react-bootstrap/Card";
 import "../../styles/JobPostings.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Button from 'react-bootstrap/Button';
-
+import Button from "react-bootstrap/Button";
+/**
+ * The SavedJobs page displays the jobs that were saved in the JobPageForSeekers.js. It is used access the saved jobs more easily.
+ *
+ * @return { Object } The page as a React component with the information of the job posts saved by the current user.
+ */
 function SavedJobs() {
-    const [postings, setPostings] = useState([]);
-  
-    useEffect(() => {
-      const postingsCollection = collection(db, "posting");
-      const q = query(postingsCollection);
-  
-      const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        const docs = [];
-        querySnapshot.forEach((doc) => {
-          docs.push({
-            id: doc.id,
-            ...doc.data(),
-          });
-        });
-        setPostings(docs);
-      });
-  
-      return () => {
-        unsubscribe();
-      };
-    }, []);
-  // Function to redirect to the "JobPostings" page
-	const handleClickJobPostings = () => {
-		window.location.href = '/JobPageForSeekers';
-	};
-	const handleClickSavedJobs = () => {
-		window.location.href = '/SavedJobs';
-	};
-    return (
-      <>
+  const [saved, Setsaved] = useState([]);
+  const [ids, Setids] = useState([]);
+  const [PostData, SetPostData] = useState([]);
+  const authUserId = auth.currentUser.uid;
+  const saveRef = collection(db, "savedPostings");
+  const postRef = collection(db, "posting");
 
-        <Container  className="container d-flex justify-content-center mx-auto">
-        <Col xs={12} sm={8}  lg={4} style={{minWidth: "30%"}}>
-				{/* This card displays the job menu block with Job Postings and Advertisements */}
-				<Card className="jobs-menu">
-					<h2> Jobs </h2>
-					<hr></hr>
-					{/* When the user clicks the "Job Postings" text, it calls handleClickJobPostings */}
-					<h4 onClick={handleClickJobPostings}style={{ color: '#888888' }}> Suggested Jobs </h4>
-					{/* Advertisements */}
-					<h4 onClick={handleClickSavedJobs}  style={{ color: '#27746a' }}> Saved Jobs </h4>
-					<br></br>
-				</Card>
-			</Col>
-            <Col xs={12} sm={12}  lg={8} >
-				<h2 style={{ color: '#555555', marginTop: '32px' }}>Your Saved Jobs</h2>
-				
-			</Col>
-          <Row>
-            
-          </Row>
-        </Container>
-      </>
-    );
-  }
-  
-  export default SavedJobs;
+  //captures the id of the job posts that the current user saved
+  useEffect(() => {
+    getDoc(doc(saveRef, authUserId)).then((connection) => {
+      Setids(connection.data().saved);
+      console.log(ids);
+    });
+  }, []);
+
+  //retrieve the data of each post
+  useEffect(() => {
+    ids.forEach((id) => {
+      getDoc(doc(postRef, id))
+        .then((post) => {
+          const { job_title, company, description, skills } = post.data();
+          const id = post.id;
+          if (!PostData.find((post1) => post1.id === id)) {
+            SetPostData((prevData) => [
+              ...prevData,
+              { id, job_title, company, description, skills },
+            ]);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  }, [ids]);
+
+  //retrieve the data associated to each saved post of the current user
+  useEffect(() => {
+    getDoc(doc(saveRef, authUserId)).then((user) => {
+      const PostSavedArray = user.data().saved;
+      const arrayPost = [];
+      PostSavedArray.forEach((id) => {
+        getDoc(doc(postRef, id)).then((other) => {
+          const { job_title, company, description, skills } = other.data();
+          const otherId = other.id;
+          const set = new Set();
+          set.add({ id, job_title, company, description, skills });
+
+          set.forEach((element) => {
+            arrayPost.push(element);
+          });
+          Setsaved(arrayPost);
+        });
+      });
+    });
+  }, []);
+
+  // Function to redirect to the "JobPostings" page
+  const handleClickJobPostings = () => {
+    window.location.href = "/JobPageForSeekers";
+  };
+  const handleClickSavedJobs = () => {
+    window.location.href = "/SavedJobs";
+  };
+  return (
+    <>
+      <Container className="container d-flex justify-content-center mx-auto">
+        <Col xs={12} sm={8} lg={4} style={{ minWidth: "30%" }}>
+          {/* This card displays the job menu block with Job Postings and Advertisements */}
+          <Card className="jobs-menu">
+            <h2> Jobs </h2>
+            <hr></hr>
+            {/* When the user clicks the "Suggested jobs" text, it calls handleClickJobPostings */}
+            <h4 onClick={handleClickJobPostings} style={{ color: "#888888" }}>
+              {" "}
+              Suggested Jobs{" "}
+            </h4>
+            {/* Saved posts */}
+            <h4 onClick={handleClickSavedJobs} style={{ color: "#27746a" }}>
+              {" "}
+              Saved Jobs{" "}
+            </h4>
+            <br></br>
+          </Card>
+        </Col>
+        <Col xs={12} sm={12} lg={8}>
+          <h2 style={{ color: "#555555", marginTop: "32px", marginLeft: "8%" }}>
+            Your Saved Jobs
+          </h2>
+          {saved.map((user) => (
+            <Col md={8} key={user.id}>
+              <Card className="mb-3 card-save">
+                <Card.Body>
+                  <Card.Title>{user.job_title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {user.company}
+                  </Card.Subtitle>
+                  <Card.Text>{user.description}</Card.Text>
+                  <Card.Text>{user.skills}</Card.Text>
+                  <Button
+                    variant="primary"
+                    style={{ backgroundColor: "#27746A" }}
+                  >
+                    Apply Now
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Col>
+        <Row></Row>
+      </Container>
+    </>
+  );
+}
+
+export default SavedJobs;
