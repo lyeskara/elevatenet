0d24c9ec774afda213a5ed7fa713202ebfc7981a
kalsij <63124857+kalsij@users.noreply.github.com>
2023-03-30 01:56:31 -0400

    Update JobPageForSeekers.js

--- frontend/src/components/Jobs/JobPageForSeekers.js
+++ frontend/src/components/Jobs/JobPageForSeekers.js
@@ -1,16 +1,6 @@
 // Importing necessary modules
-import React, { useEffect, useState, useRef } from "react";
-import {
-	collection,
-	query,
-	where,
-	getDoc,
-	doc,
-	onSnapshot,
-	getDocs,
-	deleteDoc,
-	updateDoc,
-} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, query, getDoc, doc, onSnapshot } from "firebase/firestore";
 import { auth, db } from "../../firebase";
 import Card from "react-bootstrap/Card";
 import "../../styles/JobPostings.css";
