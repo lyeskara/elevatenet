d74cb74e004d73e7c6b5ec529c1ea23d1b5d2b2c
fatema-a <fatema_n_a@hotmail.com>
2023-03-31 20:54:55 -0400

    Update ConnectionPage.test.js
    updating test for connectionpage

--- frontend/src/components/connection/ConnectionPage.test.js
+++ frontend/src/components/connection/ConnectionPage.test.js
@@ -1,69 +1,73 @@
-/**
- * @jest-environment jsdom
- */
-
-test('use jsdom in this test file', () => {
-    const element = document.createElement('div');
-    expect(element).not.toBeNull();
-  });
-
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import ConnectionPage from './ConnectionPage';
 
 describe('ConnectionPage', () => {
   beforeEach(() => {
-    // Mock Firebase functions used in the component
+    // Mock Firebase auth and db
     jest.mock('../../firebase', () => ({
       auth: {
-        currentUser: {
-          uid: 'user123',
-        },
+        currentUser: { uid: 'test-user' },
       },
-      db: {
-        collection: jest.fn(() => ({
-          doc: jest.fn(() => ({
-            get: jest.fn(() =>
-              Promise.resolve({
-                exists: true,
-                data: () => ({
-                  connections: ['user456'],
-                }),
-              })
-            ),
-            update: jest.fn(),
-          })),
-        })),
-        getDoc: jest.fn(() =>
-          Promise.resolve({
-            exists: true,
-            data: () => ({
-              firstName: 'John',
-              lastName: 'Doe',
-            }),
-          })
-        ),
-      },
+      db: {},
     }));
   });
 
   afterEach(() => {
-    jest.resetAllMocks();
+    // Reset Firebase mock
+    jest.resetModules();
   });
 
-  test('displays user connections and deletes connection on click', async () => {
+  it('renders the "My Network" sidebar', () => {
     render(<ConnectionPage />);
+    const sidebar = screen.getByRole('navigation');
+    expect(sidebar).toBeInTheDocument();
+    expect(sidebar).toHaveTextContent('My Network');
+    expect(sidebar).toContainHTML('<a href="/connections"><img src="clarity_node.png" alt="node"> Connections</a>');
+    expect(sidebar).toContainHTML('<a href="/GroupNetwork"><img src="group.png" alt="node"> Groups</a>');
+    expect(sidebar).toContainHTML('<a href="/Event"><img src="event.png" alt="node"> Events</a>');
+  });
+
+  it('renders the "Pending Requests" card', () => {
+    render(<ConnectionPage />);
+    const pendingRequests = screen.getByRole('heading', { name: 'Pending Requests' });
+    expect(pendingRequests).toBeInTheDocument();
+    expect(pendingRequests.closest('.card')).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'View all requests' })).toHaveAttribute('href', '/requests');
+  });
+
+  it('renders the "My Connections" card with user profiles', async () => {
+    // Mock Firebase db collection and documents
+    const connectionDoc = { data: () => ({ connections: ['user1', 'user2'] }) };
+    const user1Doc = { id: 'user1', data: () => ({ firstName: 'John', lastName: 'Doe', profilePicUrl: 'test.jpg' }) };
+    const user2Doc = { id: 'user2', data: () => ({ firstName: 'Jane', lastName: 'Doe' }) };
+    jest.mock('firebase/firestore', () => ({
+      collection: () => ({
+        doc: () => ({
+          get: jest.fn().mockResolvedValueOnce(connectionDoc).mockResolvedValueOnce(user1Doc).mockResolvedValueOnce(user2Doc),
+          update: jest.fn(),
+        }),
+      }),
+    }));
+
+    render(<ConnectionPage />);
+    const connectionsCard = screen.getByRole('heading', { name: 'My Connections' }).closest('.card');
+    expect(connectionsCard).toBeInTheDocument();
 
     // Wait for connections to load
-    await waitFor(() => expect(screen.getByText('John Doe')).toBeInTheDocument());
+    await screen.findByText('John Doe');
+    await screen.findByText('Jane Doe');
 
-    // Check that connections are displayed
-    expect(screen.getByText('My Connections')).toBeInTheDocument();
-    expect(screen.getByText('John Doe')).toBeInTheDocument();
+    // Check rendered profiles
+    const user1Profile = screen.getByRole('img', { name: 'John' });
+    expect(user1Profile).toHaveAttribute('src', 'test.jpg');
+    const user2Profile = screen.getByRole('img', { name: 'Jane' });
+    expect(user2Profile).toHaveAttribute('src', 'test.gif');
 
-    // Click "Delete Connection" button and check that it disappears
-    userEvent.click(screen.getByText('Delete Connection'));
-    expect(screen.queryByText('John Doe')).not.toBeInTheDocument();
+    // Test remove button
+    const user1RemoveButton = user1Profile.closest('.containRequest').querySelector('button');
+    user1RemoveButton.click();
+    expect(connectionDoc.data().connections).toEqual(['user2']);
+    expect(user1Doc.update).toHaveBeenCalledWith({ connections: [] });
   });
 });
