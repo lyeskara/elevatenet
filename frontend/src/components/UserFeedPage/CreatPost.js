19c3719b4fcbf675d81d588a365fc7bec77c0901
LeonZhang7 <zhang.leon07@gmail.com>
2023-04-07 23:04:19 -0400

    slightly formatting the code

--- frontend/src/components/UserFeedPage/CreatPost.js
+++ frontend/src/components/UserFeedPage/CreatPost.js
@@ -89,9 +89,9 @@ function CreatPost() {
       } else {
         posts_data.push(post)
         updateDoc(doc(postsCollectionRef, currentId), { "posts": posts_data })
-        // Navigate to the feed page
-        navigate("/feed");        
       }
+      // Navigate to the feed page
+      navigate("/feed"); 
     }
 
   };
