a64f35fb69bfaae47918ad2d0f9430e5d8083e7d
kalsij <63124857+kalsij@users.noreply.github.com>
2023-04-13 23:19:00 -0400

    update

--- frontend/src/components/ProfileInfo/Profile.js
+++ frontend/src/components/ProfileInfo/Profile.js
@@ -14,335 +14,330 @@ import { getStorage, ref, getDownloadURL, getMetadata } from "firebase/storage";
  * Profile loads values stored in the data base and allows us to view them in a styled page.
  */
 function Profile() {
-	const [user, setUser] = useState({});
-	const [numConnections, setNumConnections] = useState(0);
-	const [profilePicURL, setProfilePicURL] = useState("");
-	const storage = getStorage();
+  const [user, setUser] = useState({});
+  const [numConnections, setNumConnections] = useState(0);
+  const [profilePicURL, setProfilePicURL] = useState("");
+  const storage = getStorage();
+  const [education, setEducation] = useState([]);
 
-	/**
-	 * getUserData gets all values pertaining to the logged in user.
-	 */
+  /**
+   * getUserData gets all values pertaining to the logged in user.
+   */
 
-	const getUserData = async () => {
-		try {
-			const userDoc = await getDoc(
-				doc(collection(db, "users_information"), auth.currentUser.uid)
-			);
+  const getUserData = async () => {
+	try {
+	  const userDoc = await getDoc(
+		doc(collection(db, "users_information"), auth.currentUser.uid)
+	  );
+  
+	  if (userDoc.exists) {
+		// Set the user state
+		setUser({ ...userDoc.data(), id: userDoc.id });
+  
+		// Get the profile picture URL from Firebase Storage
+		const storageRef = ref(
+		  storage,
+		  `profilepics/${auth.currentUser.uid}/profilePic`
+		);
+  
+		// Check if the profile picture exists in Firebase Storage
+		const metadata = await getMetadata(storageRef);
+		const downloadURL = await getDownloadURL(storageRef);
+  
+		// Set the profile picture URL state
+		setProfilePicURL(downloadURL);
+  
+		// Get the education data
+		const educationData = userDoc.data().Education;
+		console.log(JSON.stringify(educationData));
 
-			if (userDoc.exists) {
-				// Set the user state
-				setUser({ ...userDoc.data(), id: userDoc.id });
+		setEducation(educationData);
+		console.log("educationData: ", educationData);
+	  } else {
+		console.log("User not found");
+	  }
+	} catch (error) {
+	  console.log(error);
+  
+	  // Set the profile picture URL state to a default value
+	  setProfilePicURL(
+		"https://firebasestorage.googleapis.com/v0/b/soen390-b027d.appspot.com/o/profilepics%2FBase%2Ftest.gif?alt=media&token=d295d8c2-493f-4c20-8fac-0ede65eaf0b6"
+	  );
+	}
+	console.log("name: " + education.School1.name)
+  };
+  
 
-				// Get the profile picture URL from Firebase Storage
-				const storageRef = ref(
-					storage,
-					`profilepics/${auth.currentUser.uid}/profilePic`
-				);
+  // Fix this code segment
 
-				// Check if the profile picture exists in Firebase Storage
-				const metadata = await getMetadata(storageRef);
-				const downloadURL = await getDownloadURL(storageRef);
+  // const getConnectionData = async () => {
+  // 	try {
+  // 		const connectionDoc = await getDoc(
+  // 			doc(collection(db, "connection", auth.currentUser.uid, "connections"))
+  // 		);
 
-				// Set the profile picture URL state
-				setProfilePicURL(downloadURL);
+  // 		if (connectionDoc.exists) {
+  // 			console.log("Connection document does exist");
+  // 			const connections = connectionDoc.connections;
+  // 			console.log(connections);
+  // 			const numConnections = connections ? connections.length : 0;
+  // 			setNumConnections(numConnections);
+  // 		} else {
+  // 			console.log("Connection document does not exist");
+  // 		}
+  // 	} catch (error) {
+  // 		console.log(error);
+  // 	}
+  // };
 
-				// Get the education data
-				await getEducationData();
-			} else {
-				console.log("User not found");
-			}
-		} catch (error) {
-			console.log(error);
+  useEffect(() => {
+    getUserData();
+    //	getConnectionData();
+  }, [auth]);
 
-			// Set the profile picture URL state to a default value
-			setProfilePicURL(
-				"https://firebasestorage.googleapis.com/v0/b/soen390-b027d.appspot.com/o/profilepics%2FBase%2Ftest.gif?alt=media&token=d295d8c2-493f-4c20-8fac-0ede65eaf0b6"
-			);
-		}
-	};
+  const downloadResume = async () => {
+    const storageRef = ref(storage, `resume/${auth.currentUser.uid}/resume`);
+    try {
+      const url = await getDownloadURL(storageRef);
+      if (url) {
+        window.open(url);
+      } else {
+        alert("Resume file not found!");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Error downloading resume file!");
+    }
+  };
 
-	const getEducationData = async () => {
-		try {
-			const educationRef = doc(
-				collection(db, "users_information", auth.currentUser.uid, "Education"),
-				auth.currentUser.uid
-			);
-			const educationDoc = await getDoc(educationRef);
+  const downloadCL = async () => {
+    const storageRef = ref(storage, `CL/${auth.currentUser.uid}/CL`);
+    try {
+      const url = await getDownloadURL(storageRef);
+      if (url) {
+        window.open(url);
+      } else {
+        alert("Cover letter file not found!");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Cover letter file not found!");
+    }
+  };
 
-			if (educationDoc.exists()) {
-				// Set the education state
-				setEducation(educationDoc.data());
-			} else {
-				console.log("Education not found for user");
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  return (
+    //Profile card
 
-	// Fix this code segment
+    <div className="contain">
+      <div style={{ textAlign: "right", marginRight: "7%", marginTop: "1%" }}>
+        <EditProfile user={user} setUser={setUser}></EditProfile>
+      </div>
+      <Row className="gap-5">
+        <Col className="col1" xs={12} md={{ span: 3, offset: 1 }}>
+          <Card className="profilecard">
+            <img
+              src={profilePicURL}
+              id="profilepic"
+              alt="Avatar"
+              className="avatar"
+            ></img>
+            <h1>
+              {user.firstName}
+              <span style={{ color: "#27746A" }}> {user.lastName}</span>
+            </h1>
+            <p style={{ color: "#A6A6A6" }}> {user.city}</p>
+            <p> {user.bio} </p>
+            <p> {user.languages} </p>
+          </Card>
 
-	// const getConnectionData = async () => {
-	// 	try {
-	// 		const connectionDoc = await getDoc(
-	// 			doc(collection(db, "connection", auth.currentUser.uid, "connections"))
-	// 		);
+          <Card className="contactcard">
+            <h5>Contact Information</h5>
+            <hr></hr>
+            <div className="email">
+              <GrMailOption />
 
-	// 		if (connectionDoc.exists) {
-	// 			console.log("Connection document does exist");
-	// 			const connections = connectionDoc.connections;
-	// 			console.log(connections);
-	// 			const numConnections = connections ? connections.length : 0;
-	// 			setNumConnections(numConnections);
-	// 		} else {
-	// 			console.log("Connection document does not exist");
-	// 		}
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// };
+              <h5
+                style={{
+                  color: "#626262",
+                }}
+              >
+                {user.email}
+              </h5>
+            </div>
+            <div className="contact">
+              <GrPhone />
+              <b> {user.contact} </b>
+            </div>
+          </Card>
+          <Card className="connectioncard">
+            <div className="Connections">
+              <h5
+                style={{
+                  color: "#626262",
+                }}
+              >
+                <u>0 Connections</u>
+              </h5>
+            </div>
+          </Card>
 
-	useEffect(() => {
-		getUserData();
-		//	getConnectionData();
-	}, [auth]);
+          <Card className="recommendationcard">
+            <h5>Recommendation</h5>
+            <hr></hr>
+            <div className="Recommendation">
+              <h5
+                style={{
+                  color: "black",
+                }}
+              >
+                <u>Jasmit Kalsi</u>
+              </h5>
+            </div>
+          </Card>
 
-	const downloadResume = async () => {
-		const storageRef = ref(storage, `resume/${auth.currentUser.uid}/resume`);
-		try {
-			const url = await getDownloadURL(storageRef);
-			if (url) {
-				window.open(url);
-			} else {
-				alert("Resume file not found!");
-			}
-		} catch (error) {
-			console.log(error);
-			alert("Error downloading resume file!");
-		}
-	};
+          <Card className="awardscard">
+            <h5>Awards</h5>
+            <hr></hr>
+            <div className="Awards">
+              <h5
+                style={{
+                  color: "black",
+                }}
+              >
+                {user.awards &&
+                  Array.isArray(user.awards) &&
+                  user.awards.map((awards) => (
+                    <div key={awards}>{user.awards}</div>
+                  ))}
+              </h5>
+            </div>
+          </Card>
+          <Card className="docscard">
+            <div className="resume">
+              <h5
+                style={{
+                  color: "#626262",
+                }}
+              >
+                Resume
+              </h5>
+              <button
+                className="btn btn-primary"
+                style={{ backgroundColor: "#27746A", borderColor: "#27746A" }}
+                onClick={downloadResume}
+              >
+                Download
+              </button>
+            </div>
+            <hr style={{ marginBottom: "6px" }}></hr>
+            <div className="coverletter">
+              <h5
+                style={{
+                  color: "#626262",
+                }}
+              >
+                Cover Letter
+              </h5>
+              <button
+                type="button"
+                className="btn btn-primary"
+                style={{ backgroundColor: "#27746A", borderColor: "#27746A" }}
+                onClick={downloadCL}
+              >
+                Download
+              </button>
+            </div>
+          </Card>
+        </Col>
 
-	const downloadCL = async () => {
-		const storageRef = ref(storage, `CL/${auth.currentUser.uid}/CL`);
-		try {
-			const url = await getDownloadURL(storageRef);
-			if (url) {
-				window.open(url);
-			} else {
-				alert("Cover letter file not found!");
-			}
-		} catch (error) {
-			console.log(error);
-			alert("Cover letter file not found!");
-		}
-	};
+        <Col xs={12} md={7}>
+          <Card className="card">
+            <h5>Work Experience</h5>
+            <hr></hr>
+            <div className="profile-desc-row">
+              <img src={person}></img>
+              <div>
+                <h3>Business Intelligence Analyst</h3>
+                <p>DODO Inc.</p>
+                <p> Feb 2022 - Present</p>
+              </div>
+            </div>
+            <hr></hr>
+            <div className="profile-desc-row">
+              <img src={person}></img>
+              <div>
+                <h3>Junior Analyst</h3>
+                <p>FOFO Inc.</p>
+                <p> Feb 2021 - Feb 2022</p>
+              </div>
+            </div>
+          </Card>
 
-	return (
-		//Profile card
+		  <Card className="educationcard">
+  <h5>Education</h5>
+  <hr></hr>
+  {user.education && Object.entries(education).map(([key, school]) => (
+  <React.Fragment key={key}>
+    <div className="profile-desc-row">
+      <img src={person} alt="person"></img>
+      <div>
+        <h3>{school.name}</h3>
+        <p>{school.startDate} - {school.endDate}</p>
+        <p>{school.major}</p>
+        {/* Here you can access the other properties of the school object */}
+      </div>
+    </div>
+    {/* Add a horizontal rule between schools */}
+    {Object.keys(user.education).indexOf(key) !== Object.keys(user.education).length - 1 && <hr />}
+  </React.Fragment>
+))}
 
-		<div className="contain">
-			<div style={{ textAlign: "right", marginRight: "7%", marginTop: "1%" }}>
-				<EditProfile user={user} setUser={setUser}></EditProfile>
-			</div>
-			<Row className="gap-5">
-				<Col className="col1" xs={12} md={{ span: 3, offset: 1 }}>
-					<Card className="profilecard">
-						<img
-							src={profilePicURL}
-							id="profilepic"
-							alt="Avatar"
-							className="avatar"
-						></img>
-						<h1>
-							{user.firstName}
-							<span style={{ color: "#27746A" }}> {user.lastName}</span>
-						</h1>
-						<p style={{ color: "#A6A6A6" }}> {user.city}</p>
-						<p> {user.bio} </p>
-						<p> {user.languages} </p>
-					</Card>
 
-					<Card className="contactcard">
-						<h5>Contact Information</h5>
-						<hr></hr>
-						<div className="email">
-							<GrMailOption />
+</Card>
 
-							<h5
-								style={{
-									color: "#626262",
-								}}
-							>
-								{user.email}
-							</h5>
-						</div>
-						<div className="contact">
-							<GrPhone />
-							<b> {user.contact} </b>
-						</div>
-					</Card>
-					<Card className="connectioncard">
-						<div className="Connections">
-							<h5
-								style={{
-									color: "#626262",
-								}}
-							>
-								<u>0 Connections</u>
-							</h5>
-						</div>
-					</Card>
 
-					<Card className="recommendationcard">
-						<h5>Recommendation</h5>
-						<hr></hr>
-						<div className="Recommendation">
-							<h5
-								style={{
-									color: "black",
-								}}
-							>
-								<u>Jasmit Kalsi</u>
-							</h5>
-						</div>
-					</Card>
+          <Card className="skillscard">
+            <h5>Skills</h5>
+            <hr></hr>
+            {user.skills && Array.isArray(user.skills) && (
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                {user.skills.map((skill) => (
+                  <span key={skill} className="skills-btn">
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            )}
+          </Card>
 
-					<Card className="awardscard">
-						<h5>Awards</h5>
-						<hr></hr>
-						<div className="Awards">
-							<h5
-								style={{
-									color: "black",
-								}}
-							>
-								{user.awards &&
-									Array.isArray(user.awards) &&
-									user.awards.map((awards) => (
-										<div key={awards}>{user.awards}</div>
-									))}
-							</h5>
-						</div>
-					</Card>
-					<Card className="docscard">
-						<div className="resume">
-							<h5
-								style={{
-									color: "#626262",
-								}}
-							>
-								Resume
-							</h5>
-							<button
-								className="btn btn-primary"
-								style={{ backgroundColor: "#27746A", borderColor: "#27746A" }}
-								onClick={downloadResume}
-							>
-								Download
-							</button>
-						</div>
-						<hr style={{ marginBottom: "6px" }}></hr>
-						<div className="coverletter">
-							<h5
-								style={{
-									color: "#626262",
-								}}
-							>
-								Cover Letter
-							</h5>
-							<button
-								type="button"
-								className="btn btn-primary"
-								style={{ backgroundColor: "#27746A", borderColor: "#27746A" }}
-								onClick={downloadCL}
-							>
-								Download
-							</button>
-						</div>
-					</Card>
-				</Col>
-
-				<Col xs={12} md={7}>
-					<Card className="card">
-						<h5>Work Experience</h5>
-						<hr></hr>
-						<div className="profile-desc-row">
-							<img src={person}></img>
-							<div>
-								<h3>Business Intelligence Analyst</h3>
-								<p>DODO Inc.</p>
-								<p> Feb 2022 - Present</p>
-							</div>
-						</div>
-						<hr></hr>
-						<div className="profile-desc-row">
-							<img src={person}></img>
-							<div>
-								<h3>Junior Analyst</h3>
-								<p>FOFO Inc.</p>
-								<p> Feb 2021 - Feb 2022</p>
-							</div>
-						</div>
-					</Card>
-
-					<Card className="educationcard">
-						<h5>Education</h5>
-						<hr></hr>
-						{user.education.map((school, index) => (
-							<React.Fragment key={index}>
-								<div className="profile-desc-row">
-									<img src={person}></img>
-									<div>
-										<h3>{school}</h3>
-									</div>
-								</div>
-								{index !== user.education.length - 1 && <hr />}
-							</React.Fragment>
-						))}
-					</Card>
-
-					<Card className="skillscard">
-						<h5>Skills</h5>
-						<hr></hr>
-						{user.skills && Array.isArray(user.skills) && (
-							<div style={{ display: "flex", flexDirection: "row" }}>
-								{user.skills.map((skill) => (
-									<span key={skill} className="skills-btn">
-										{skill}
-									</span>
-								))}
-							</div>
-						)}
-					</Card>
-
-					<Card className="courses">
-						<h5>Courses</h5>
-						<hr></hr>
-						{user.courses &&
-							Array.isArray(user.courses) &&
-							user.courses.map((courses) => <div key={courses}>{courses}</div>)}
-					</Card>
-					<Card className="projects">
-						<h5>Projects</h5>
-						<hr></hr>
-						{user.courses &&
-							Array.isArray(user.projects) &&
-							user.projects.map((projects) => (
-								<div key={projects}>{projects}</div>
-							))}
-					</Card>
-					<Card className="volunteering">
-						<h5>Volunteering</h5>
-						<hr></hr>
-						{user.volunteering &&
-							Array.isArray(user.volunteering) &&
-							user.volunteering.map((volunteering) => (
-								<div key={volunteering}>{volunteering}</div>
-							))}
-					</Card>
-				</Col>
-			</Row>
-		</div>
-	);
+          <Card className="courses">
+            <h5>Courses</h5>
+            <hr></hr>
+            {user.courses &&
+              Array.isArray(user.courses) &&
+              user.courses.map((courses) => <div key={courses}>{courses}</div>)}
+          </Card>
+          <Card className="projects">
+            <h5>Projects</h5>
+            <hr></hr>
+            {user.courses &&
+              Array.isArray(user.projects) &&
+              user.projects.map((projects) => (
+                <div key={projects}>{projects}</div>
+              ))}
+          </Card>
+          <Card className="volunteering">
+            <h5>Volunteering</h5>
+            <hr></hr>
+            {user.volunteering &&
+              Array.isArray(user.volunteering) &&
+              user.volunteering.map((volunteering) => (
+                <div key={volunteering}>{volunteering}</div>
+              ))}
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 export default Profile;
