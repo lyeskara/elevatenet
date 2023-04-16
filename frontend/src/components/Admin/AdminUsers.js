020a2ddec6e15bdd6df4df2f07cdf27f15b03d16
LeonZhang7 <zhang.leon07@gmail.com>
2023-04-16 00:17:33 -0400

    Making function to display all users
    Making function to display all users

--- frontend/src/components/Admin/AdminUsers.js
+++ frontend/src/components/Admin/AdminUsers.js
@@ -1,8 +1,28 @@
-function AdminUsers(){
-    return(
-        <h1>Admin Users</h1>
-    )
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/auth';
 
+function AdminUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const listUsersResult = await firebase.auth().listUsers();
+      setUsers(listUsersResult.users);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Admin Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.uid}>{user.uid}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default AdminUsers
+export default AdminUsers;
