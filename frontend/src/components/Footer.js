1d5ebd8e749b9898e2e982465655dcdd00f61a3a
fatema-a <fatema_n_a@hotmail.com>
2023-04-18 02:38:18 -0400

    fixing language component bug
    reactivity is now fixed only minor styling needs to be added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,21 +3,17 @@ import React, { useEffect } from 'react';
 function Footer() {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src =
-      'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
     script.async = true;
     document.body.appendChild(script);
-
-    window.googleTranslateElementInit = () => {
-      new window.google.translate.TranslateElement(
-        { pageLanguage: 'en' },
-        'google_translate_element'
-      );
-    };
   }, []);
 
-  return <div id="google_translate_element"></div>;
+  return (
+    <footer>
+      
+      <div id="google_translate_element"></div>
+    </footer>
+  );
 }
 
 export default Footer;
-
