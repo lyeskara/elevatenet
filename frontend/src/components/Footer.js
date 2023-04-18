af019e89bb3708a7fde1bfb640f957c286625add
fatema-a <fatema_n_a@hotmail.com>
2023-04-18 03:38:02 -0400

    adding final fixes to the language changes
    how the footer is reactive and  the change language button only appears once

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,12 @@
 import React, { useEffect } from 'react';
 
 function Footer() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
+
 
   return (
     <footer>
       
-      <div id="google_translate_element"></div>
+      <h1>foot</h1>
     </footer>
   );
 }
