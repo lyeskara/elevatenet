0d067bf95a5d6c73fabe18115118936eadc8bd4d
fatema-a <fatema_n_a@hotmail.com>
2023-04-18 02:24:42 -0400

    adding the script to the footer
    adding change language script to the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,23 @@
-function Footer(){
+import React, { useEffect } from 'react';
 
-    return(
-        <h1>FOOTER</h1>
-    )
+function Footer() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.googleTranslateElementInit = () => {
+      new window.google.translate.TranslateElement(
+        { pageLanguage: 'en' },
+        'google_translate_element'
+      );
+    };
+  }, []);
+
+  return <div id="google_translate_element"></div>;
 }
+
 export default Footer;
+
