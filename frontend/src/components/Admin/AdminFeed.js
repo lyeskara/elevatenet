de1d2883e45d52187f5db173af4eabb3113d941e
LeonZhang7 <zhang.leon07@gmail.com>
2023-04-19 02:05:14 -0400

    Only Admin account can access admin user post feed page
    Only Admin account can access admin user post feed page
    
    Co-Authored-By: fatema-a <78175202+fatema-a@users.noreply.github.com>

--- frontend/src/components/Admin/AdminFeed.js
+++ frontend/src/components/Admin/AdminFeed.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, doc, updateDoc, deleteDoc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase";
+import { auth, db } from "../../firebase";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-
+import { Link } from 'react-router-dom';
 
 function AdminFeed() {
   const [posts, setPosts] = useState([]);
@@ -69,62 +69,75 @@ function AdminFeed() {
   function goToUser(){
     window.location.href = "/AdminUsers";
   }
+  //CHECK IF THE USER CONNECTED IS AN ADMIN
+  const currentUser = auth.currentUser;
+	if (currentUser?.uid === '361FbyTxmmZqCT03kGd25kSyDff1') {
+		return (
+      <Container>
+        <Row>
+          {/* This card displays the job menu block with Job Postings and Advertisements */}
+          <Card className="jobs-menu">
+            <h2> Manage </h2>
+            <hr></hr>
+            {/* When the user clicks the "Job Postings" text, it calls handleClickJobPostings */}
+            <h4 onClick={goToAdmin} style={{ color: "#888888" }}>
+              {" "}
+              Job Postings{" "}
+            </h4>
+            {/* Feed Posts */}
+            <h4
+              onClick={goToFeedPosts}
+              style={{ color: "#27746a" }}
+            >
+              {" "}
+              Feed Posts{" "}
+            </h4>
+                      <h4
+              onClick={goToUser}
+              style={{ color: "#888888" }}
+            >
+              {" "}
+              Users{" "}
+            </h4>
+            <br></br>
+          </Card>
+        </Row>
+        <Row>
+          {posts.map((post) => (
+            <Col key={post.id} sm={12} md={6} lg={4} className="mb-4">
+              <Card>
+                <h5>{post.id}</h5>
+                <h6>Created by: {post.postTexts[0]?.created_by ?? 'N/A'}</h6>
+                {console.log(post.postTexts[0])}
+                <Card.Body>
+                  {post.postTexts.map((text) => (
+                    <div key={text.id} className="d-flex justify-content-between">
+                      <Card.Text>{text.text}</Card.Text>
+                      <Button
+                        variant="outline-danger"
+                        size="sm"
+                        onClick={() => handleDelete(post.id, text.id)}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  ))}
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+	
   return (
-    <Container>
-      <Row>
-        {/* This card displays the job menu block with Job Postings and Advertisements */}
-				<Card className="jobs-menu">
-					<h2> Manage </h2>
-					<hr></hr>
-					{/* When the user clicks the "Job Postings" text, it calls handleClickJobPostings */}
-					<h4 onClick={goToAdmin} style={{ color: "#888888" }}>
-						{" "}
-						Job Postings{" "}
-					</h4>
-					{/* Feed Posts */}
-					<h4
-						onClick={goToFeedPosts}
-						style={{ color: "#27746a" }}
-					>
-						{" "}
-						Feed Posts{" "}
-					</h4>
-                    <h4
-						onClick={goToUser}
-						style={{ color: "#888888" }}
-					>
-						{" "}
-						Users{" "}
-					</h4>
-					<br></br>
-				</Card>
-      </Row>
-      <Row>
-        {posts.map((post) => (
-          <Col key={post.id} sm={12} md={6} lg={4} className="mb-4">
-            <Card>
-              <h5>{post.id}</h5>
-              <h6>Created by: {post.postTexts[0]?.created_by ?? 'N/A'}</h6>
-              {console.log(post.postTexts[0])}
-              <Card.Body>
-                {post.postTexts.map((text) => (
-                  <div key={text.id} className="d-flex justify-content-between">
-                    <Card.Text>{text.text}</Card.Text>
-                    <Button
-                      variant="outline-danger"
-                      size="sm"
-                      onClick={() => handleDelete(post.id, text.id)}
-                    >
-                      Delete
-                    </Button>
-                  </div>
-                ))}
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+  <div>
+    <h1>You do not have permission to view this page.</h1>
+    <Link to="/" className="btn btn-primary" style={{ backgroundColor: '#27746A' }}>
+    Go to back to main page
+    </Link>
+  </div>
   );
   
 }
