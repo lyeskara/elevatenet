4daca31566748a357762de53230786b8f50fc3d2
fatema-a <78175202+fatema-a@users.noreply.github.com>
2023-04-24 03:57:18 -0400

    Revert "merging main into notifs"

--- frontend/src/components/Settings/NameSetting.js
+++ frontend/src/components/Settings/NameSetting.js
@@ -15,12 +15,9 @@ import backward from ".././../images/backward.png";
 import { auth, db } from "../../firebase";
 import {
   collection,
-  getDocs,
   getDoc,
   doc,
   updateDoc,
-  query,
-  where,
   setDoc,
 } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
@@ -33,65 +30,10 @@ function NameSetting() {
   const [user, setUser] = useState({});
   const [updatedUser, setUpdatedUser] = useState(null); // create a copy of the user object using useState
   const [showModal, setShowModal] = useState(false);
-  const [userType, setUserType] = useState(null); // add state to store user type
-  const userr = auth.currentUser;
-  const getUserType = async () => {
-    try {
-       // Get the current user's email
-    const email = auth.currentUser.email;
-    if (!email) {
-      console.log("No email found for current user.");
-      return;
-    }
-      // Check if the email belongs to a recruiter
-    const recruiterQuerySnapshot = await getDocs(
-      query(collection(db, "recruiters_informations"), where("email", "==", email))
-    ); 
-    if (!recruiterQuerySnapshot.empty) {
-      setUserType("recruiter");
-      console.log("recruiter");
-      return;
-    }
-      // Check if the email belongs to a job seeker
-      const userQuerySnapshot = await getDocs(
-        query(collection(db, "users_information"), where("email", "==", email))
-      );
-      if (!userQuerySnapshot.empty) {
-        setUserType("job seeker");
-        console.log("job seeker");
-        return;
-      }
-      console.log({userType});
-      console.log("No user found with the email: ", email);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
-  useEffect(() => {
-    getUserType();
-  }, [userr]);
-
-  const getUserDataRecruiter = async () => {
-    try {
-      const recruiterDoc = await getDoc(
-        doc(collection(db, "recruiters_informations"), auth.currentUser.uid)
-      );
-  
-      if (recruiterDoc.exists) {
-        // Set the user state
-        setUser({ ...recruiterDoc.data() });
-      } else {
-        console.log("User not found");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
   const getUserData = async () => {
     try {
-
-        const userDoc = await getDoc(
+      const userDoc = await getDoc(
         doc(collection(db, "users_information"), auth.currentUser.uid)
       );
 
@@ -101,7 +43,6 @@ function NameSetting() {
       } else {
         console.log("User not found");
       }
-      
     } catch (error) {
       console.log(error);
     }
@@ -112,8 +53,7 @@ function NameSetting() {
     e.preventDefault();
 
     try {
-      if(userType == "job seeker"){
-        const userRef = doc(
+      const userRef = doc(
         collection(db, "users_information"),
         auth.currentUser.uid
       );
@@ -131,27 +71,6 @@ function NameSetting() {
       // Reset the updatedUser state
       setUpdatedUser(null);
       setShowModal(true);
-      }
-      else{
-        const recruitRef = doc(
-          collection(db, "recruiters_informations"),
-          auth.currentUser.uid
-        );
-  
-        await updateDoc(recruitRef, {
-          firstName: updatedUser.firstName || user.firstName,
-          lastName: updatedUser.lastName || user.lastName,
-          city: updatedUser.city || user.city,
-        });
-  
-        // Update the user state with the new data
-        setUser({ ...user, ...updatedUser });
-  
-        // Reset the updatedUser state
-        setUpdatedUser(null);
-        setShowModal(true);
-      }
-      
     } catch (error) {
       console.log(error);
     }
@@ -164,14 +83,8 @@ function NameSetting() {
   };
 
   useEffect(() => {
-    if (userType === 'job seeker') {
-      getUserData();
-    }
-    if (userType === 'recruiter') {
-      // Call getUserData with recruiter data here
-      getUserDataRecruiter();
-    }
-  }, [userType]);
+    getUserData();
+  }, []);
 
   const handleClose = () => setShowModal(false); // Add handleClose function
 
@@ -242,9 +155,7 @@ function NameSetting() {
                 className="mb-3"
                 controlId="exampleForm.ControlInput1"
               >
-                {userType === "job seeker" && (
-                  <>
-                  <Form.Label>Contact Number</Form.Label>
+                <Form.Label>Contact Number</Form.Label>
                 <Form.Control
                   name="contact"
                   type="text"
@@ -252,9 +163,6 @@ function NameSetting() {
                   onChange={update}
                   autoFocus
                 />
-                </>
-                )}
-                
               </Form.Group>
 
               <Button
